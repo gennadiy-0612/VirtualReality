c1f1e6b3d0b414233e7ef4601941efa9593ab57f
Genn <intval@i.ua>
2021-11-19 18:48:03 +0200

    start

--- system/themes/stockholm.js
+++ system/themes/stockholm.js
@@ -43,14 +43,15 @@ shch.watch = {
 
 shch.LoadFunc = function () {
 
-    shch.watch1 = new shch.addDetect();
-    shch.watch1.checkVision(shch.watch.screen1);
+    // shch.watch1 = new shch.addDetect();
+    // shch.watch1.checkVision(shch.watch.screen1);
 
-    shch.watch2 = new shch.addDetect();
-    shch.watch2.checkVision(shch.watch.screen2);
-
-    shch.watch3 = new shch.addDetect();
-    shch.watch3.checkVision(shch.watch.screen3);
+    // shch.watch2 = new shch.addDetect();
+    // shch.watch2.checkVision(shch.watch.screen2);
+    //
+    // shch.watch3 = new shch.addDetect();
+    // shch.watch3.checkVision(shch.watch.screen3);
+    alert('ppppppppppp')
 }
 
 window.addEventListener('load', shch.LoadFunc);
