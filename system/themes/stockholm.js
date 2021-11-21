060d1477f50d468c6111db0f023d7a7cebbc556a
Genn <intval@i.ua>
2021-11-21 13:08:55 +0200

    start

--- system/themes/stockholm.js
+++ system/themes/stockholm.js
@@ -13,7 +13,7 @@ shch.addDetect = function () {
 
         function vdHandler(els) {
             els.forEach((data) => {
-                if (data.intersectionRatio > .01) {
+                if (data.intersectionRatio > .09) {
                     data.target.classList.add(init.animationName);
                     console.log(init.selector);
                     console.log(data.target);
