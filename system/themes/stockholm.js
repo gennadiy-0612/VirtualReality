215f4c293b0d64a76a6604e35941908fd3392548
Genn <intval@i.ua>
2021-11-21 15:43:02 +0200

    start

--- system/themes/stockholm.js
+++ system/themes/stockholm.js
@@ -43,7 +43,7 @@ shch.InSlide = {
         this.changeDisplay();
     },
     changeDisplay: function () {
-        console.log(this.display);
+        console.log(this);
         this.frameIn[this.display].classList('actInSlide');
     }
 }
