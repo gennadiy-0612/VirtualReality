63f328ce599566d9f8001908019120c24de3f112
Genn <intval@i.ua>
2021-11-21 20:08:37 +0200

    start

--- system/themes/stockholm.js
+++ system/themes/stockholm.js
@@ -60,9 +60,8 @@ shch.InSlide = {
     changeDisplay: function () {
         this.currentDisplay = this.frameIn[this.display];
         this.frameIn[0].setAttribute('src', this.frameIn[this.display].getAttribute('data-src'));
-
         this.addEv = 1;
-        if (this.addEv) this.frameIn[0].addEventListener('load', this.addVision)
+        if (this.addEv) this.frameIn[0].addEventListener('load', this.addVision);
         this.addEv = 0;
     },
     addVision: function () {
